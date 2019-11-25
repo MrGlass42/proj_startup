96b2332f3e535b31db5e9db9acec02b84a0a7f46
Carlos Eduardo Guimarães de Souza <43711772+CarlosEduardoGui@users.noreply.github.com>
2019-11-24 22:06:08 -0300

    Adicionado a visão do investidor

---
+++ build/web/scripts/cadastro_investidor.js
@@ -0,0 +1,179 @@
+jQuery(document).ready(function () {
+    buscar_empresas();
+    
+    jQuery("#add_telefone").on('click', function () {
+        if (jQuery("#telefone_digitado").val() == '') {
+            iziToast.error({
+                message: "<strong>Digite um Número !!!</strong>"
+            });
+            return;
+        }
+
+        var telefone_digitado = jQuery("#telefone_digitado").val();
+
+        var telefones_adicionados = jQuery("#telefones").val();
+
+        telefones_adicionados += "," + telefone_digitado;
+
+        jQuery("#telefones").val(telefones_adicionados);
+        jQuery("#telefone_digitado").val("");
+
+        iziToast.success({
+            message: "<strong>Telefone Adicionado com Sucesso !!!</strong>"
+        });
+    });
+
+    jQuery(document).on("click", ".excluir_telefone", function () {
+        var id_telefone = jQuery(this).attr("id_telefone");
+        jQuery(this).remove();
+
+        excluirTelefone(id_telefone);
+    });
+    
+    jQuery("#btn_cadastro_investidor").on("click", function(e) {
+        e.preventDefault();
+        
+        if(jQuery("#senha").val() === jQuery("#confirmarSenha").val()) {
+            jQuery("#form_cadastro_investidor").submit();
+            
+            setTimeout(function() {
+                window.location("index.html");
+            }, 500);
+        } else {
+            iziToast.error({
+                message: "<strong>Senhas diferentes</strong>"
+            });
+        }
+    });
+
+});
+
+
+function buscar_empresas() 
+{
+    jQuery.ajax({
+        url: 'http://localhost:8082/proj_startup/controle_startup',
+        type: 'POST',
+        dataType: "json",
+        data: {
+            opcao: 5,
+            chaveHash: "Investidor"
+        },
+        beforeSend: function () {
+            console.log("Iniciando Requisição Para Capturar as Empresas Cadastradas...");
+        },
+        complete: function (e, xhr, result) {
+            if (e.readyState == 4 && e.status == 200) {
+                try { //Converte a resposta HTTP JSON em um objeto JavaScript
+                    var resposta_controle = JSON.parse(e.responseText); //Combo OS
+
+                } catch (err) {
+                    console.log("Erro na conversão do objeto JSON");
+                }
+                if (resposta_controle != null) {
+                    console.log(resposta_controle);
+                    
+                    var dados = resposta_controle.dados;
+
+                    var htmlTabela = '';
+                    for (var i = 0; i < dados.length; i++) {
+                        htmlTabela += '<tr><td>' + dados[i].nomeFantasia + '</td>';
+                        htmlTabela += '<td>' + dados[i].CNPJ + '</td>';
+                        htmlTabela += '<td>' + dados[i].email + '</td>';
+                        htmlTabela += '<td>' + dados[i].categoria.categoria + '</td></tr>';
+                        
+                    }
+                    console.log(htmlTabela)
+                    jQuery("#tbody_tabela_listagem_empresas").html(htmlTabela);
+
+                } else {
+                    console.log("Resposta nula do servidor...");
+                }
+            } else {
+                console.log("Erro na comunicação com o servidor...");
+            }
+
+        }
+});
+}
+
+
+function buscar_empresa(id_empresa) {
+jQuery.ajax({
+        url: 'http://localhost:8082/proj_startup/controle_startup',
+        type: 'POST',
+        dataType: "json",
+        data: {
+            opcao: 3,
+            id: id_empresa,
+            chaveHash: "Empresa"
+        },
+        beforeSend: function () {
+            console.log("Iniciando Requisição Para Buscar Dados Da Empresa...");
+        },
+        complete: function (e, xhr, result) {
+            if (e.readyState == 4 && e.status == 200) {
+                try { //Converte a resposta HTTP JSON em um objeto JavaScript
+                    var resposta_controle = JSON.parse(e.responseText); //Combo OS
+
+                } catch (err) {
+                    console.log("Erro na conversão do objeto JSON");
+                }
+                if (resposta_controle != null) {
+
+                    console.log(resposta_controle);
+                    
+                    var dados = resposta_controle.dados;
+
+                    jQuery("#id_startup").val(dados.id);
+                    jQuery("#email").val(dados.email);
+                    jQuery("#nomeFantasia").val(dados.nomeFantasia);
+                    jQuery("#razaoSocial").val(dados.razaoSocial);
+                    jQuery("#cnpj").val(dados.CNPJ);
+                    jQuery("#categoria").val(dados.categoria.categoria);
+                    jQuery("#complemento").val(dados.endereco.complemento);
+                    jQuery("#logradouro").val(dados.endereco.logradouro);
+                    jQuery("#numero").val(dados.endereco.numero);
+                    jQuery("#cidade").val(dados.endereco.cidade.nome);
+                    jQuery("#estado").val(dados.endereco.cidade.estado.nome);
+                    jQuery("#pais").val(dados.endereco.cidade.estado.pais.nome);
+
+                    jQuery(".campos_senha").hide();
+
+                    var telefones = dados.telefones;
+                    var htmlTelefones = '';
+                    for (var i = 0; i < telefones.length; i++) {
+                        htmlTelefones += '<p><a style="cursor: pointer;" class="excluir_telefone" id_telefone="' + telefones[i].id + '">' + telefones[i].numero + ' - </a></p>'
+                    }
+
+                    jQuery("#coluna_mostrar_teleofones").html(htmlTelefones);
+                } else {
+                    console.log("Resposta nula do servidor...");
+                }
+            } else {
+                console.log("Erro na comunicação com o servidor...");
+            }
+
+        }
+
+
+    });
+    }
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+
+    for (var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        hash[1] = unescape(hash[1]);
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+
+    return vars;
+}
+
+
